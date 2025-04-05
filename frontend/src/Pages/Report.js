2e373c6f4b0a4352d209e5002aa8a3de18a85313
PriyaShah012 <shah.priya1202@gmail.com>
2025-04-06 09:15:35 +1000

    refined metrics components

--- frontend/src/Pages/Report.js
+++ frontend/src/Pages/Report.js
@@ -1,4 +1,4 @@
-import '../Css/Ontology.css';
+import '../Css/Report.css';
 import React from 'react';
 import AppHeader from './Components/AppHeader';
 import AppNavigator from './Components/AppNavigator';
@@ -10,9 +10,40 @@ function Report() {
       <div className='Body'>
         <AppNavigator />
         <div className="content">
-          <div>
-            <h1>Reporting of the elements</h1>
-            <p>Dive into our initial analysis model</p>
+          <div className='allContent'>
+            <div className="mainMetrics">
+                <div className='metricRow1'>
+                    <div className='metric1'>
+                        <p className='metricTitle'>Environmental Risk</p>
+                        <div className='ERContent'>ERContent</div>
+                    </div>
+                    <div className='metric2'>
+                        <p className='metricTitle'>Social Risk</p>
+                        <div className='SRContent'>SRContent</div>
+                    </div>
+                    <div className='metric3'>
+                        <p className='metricTitle'>Governance Risk</p>
+                        <div className='GRContent'>GRContent</div>
+                    </div>
+                </div>
+                <div className='metricRow2'>
+                    <div className='metric4'>
+                        <p className='metricTitle'>EO</p>
+                        <div className='EOContent'>EOContent</div>
+                    </div>
+                    <div className='metric5'>
+                        <p className='metricTitle'>SO</p>
+                        <div className='SOContent'>SOContent</div>
+                    </div>
+                    <div className='metric6'>
+                        <p className='metricTitle'>GO</p>
+                        <div className='GOContent'>GOContent</div>
+                    </div>
+                </div>
+            </div>
+            <div className='calculatedPanel'>
+                <p>Calculation comes here</p>
+            </div>
           </div>
         </div>
       </div>
