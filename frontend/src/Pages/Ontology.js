9fe4b1e691e6b7d5af39c6443ea32bb093c7ce7b
z5078579 <z5078579@unsw.edu.au>
2025-04-20 16:39:31 +1000

    removed testing file

--- frontend/src/Pages/Ontology.js
+++ frontend/src/Pages/Ontology.js
@@ -1,32 +1,26 @@
 import '../Css/Ontology.css';
 import React from 'react';
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 import AppHeader from './Components/AppHeader';
 import AppNavigator from './Components/AppNavigator';
 
 function Ontology() {
-
-      useEffect(() => {
-          fetch('http://localhost:3902/top_5')
-              .then(response => response.json())
-              // .then(data => setPillars(data))
-              .catch(error => console.error('Error fetching environment risk sub categories:', error));
-      });
-  return (
-    <div className="App">
-      <AppHeader />
-      <div className='Body'>
-        <AppNavigator />
-        <div className="content">
-          <div>
-            <h1>Ontology</h1>
-            <p>View our primary ontology</p>
-          </div>
-          <img src="/ontologyExample.png" alt="dummy example ontology" height={210} width={370} />
+      
+    return (
+        <div className="App">
+            <AppHeader />
+            <div className='Body'>
+                <AppNavigator />
+                <div className="content">
+                    <div>
+                        <h1>Ontology</h1>
+                        <p>View our primary ontology</p>
+                    </div>
+                    <img src="/ontologyExample.png" alt="dummy example ontology" height={210} width={370} />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Ontology;
